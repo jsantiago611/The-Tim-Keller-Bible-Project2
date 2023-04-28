d85ce2324bc57f82136cb41668a0dc6b038dee23
CodeSandbox <support@codesandbox.io>
2023-04-28 15:27:02 +0000

    Initial commit

---
+++ docs/intro.md
@@ -0,0 +1,9 @@
+---
+sidebar_position: 1
+---
+
+# A Study Bible full of notes from Tim Keller's Sermons
+
+Hi!
+
+This site is not affiliated with Tim Keller or gospel in Life. 
---
+++ docusaurus.config.js
@@ -0,0 +1,107 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Discover Tim Keller's best ideas",
+  tagline:
+    "The Keller Bible Project is an effort to infuse Tim Keller's sermons into a Study BIble format so that you can see Keller's ideas alongside the Bible text.",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "jsantiago611", // Usually your GitHub org/user name.
+  projectName: "The Keller Study Bible", // Usually your repo name.
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "The Keller Study Bible Project",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Study Bible",
+          },
+          { to: "/blog", label: "Sermon Notes", position: "left" },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Study Bible",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Twitter",
+                href: "https://twitter.com/jsantiago611",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Sermon Notes",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} <span style={{ fontFamily:"soehne-ikon" }}>↯ </span>Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
